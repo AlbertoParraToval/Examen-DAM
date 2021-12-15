b9eb38f0b74659c9130647626f928c01fbe587c7
Alberto Parra Toval <91873665+AlbertoParraToval@users.noreply.github.com>
2021-12-15 14:35:33 +0100

    Delete javascripts directory

--- javascripts/javascript.js
+++
@@ -1,9 +0,0 @@
-//**** PAGINA DEL JAVASCRIPT */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-        }
-}
